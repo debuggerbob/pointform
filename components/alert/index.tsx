cada6e878ed86b242b391e6fcd8ca75791cb8b72
Ruthvik <ruthvik1@protonmail.com>
2021-04-27 18:21:11 +0530

    update alert component

--- components/alert/index.tsx
+++ components/alert/index.tsx
@@ -1,4 +1,3 @@
-import styles from "./index.module.scss";
 import { useState } from 'react'
 
 interface Props {
@@ -7,18 +6,19 @@ interface Props {
 }
 
 export const Alert: React.FC<Props> = ({ alertText, alertType }) => {
-    const [show, setShow] = useState(true)
-    let type, display
-    { alertType === "error" ? type = styles.error : type = styles.default }
-    { show ? display = styles.show : display = styles.none }
-    return (
-        <div className={`${styles.alert} ${type} ${display}`}>
-            <h3 className={styles.alert__text}>{alertText}</h3>
-            <button className={styles.alert__close} onClick={() => setShow(false)} >
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#333c48"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
-            </button>
-        </div>
-    )
+    const [alert, setAlert] = useState(true)
+    if(alert)
+        return (
+            <div className="mx-auto mt-6 mb-4">
+                <div className="bg-red-100 border border-red-400 text-gray-800 px-4 py-3 rounded relative" role="alert">
+                    <span className="block sm:inline">{alertText}</span>
+                    <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg onClick={() => setAlert(false)}  className="fill-current h-6 w-6 text-gray-800" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                    </span>
+                </div>
+            </div>
+        )
+    else return <></>
 }
 
 export default Alert
