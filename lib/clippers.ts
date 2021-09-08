475d6ed96e79e35011422dce283d3ac88b8fef4c
RV <debuggerbob@gmail.com>
2021-09-08 16:34:05 +0530

    add presence check to input parameters

--- lib/clippers.ts
+++ lib/clippers.ts
@@ -1,26 +1,29 @@
 export const objectClipper = (inputObject, requiredFields) => {
-    let keys = Object.keys(inputObject)
+    if(inputObject) {
+        let keys = Object.keys(inputObject)
    
-    let unusedFields = []
+        let unusedFields = []
+        
+        keys.forEach(key => {
+            if(!requiredFields.includes(key)) {
+                unusedFields.push(key)
+            }
+        })
     
-    keys.forEach(key => {
-        if(!requiredFields.includes(key)) {
-            unusedFields.push(key)
-        }
-    })
-
-    unusedFields.forEach(unusedField => {
-      delete inputObject[unusedField]
-  })
-    
-    return inputObject
+        unusedFields.forEach(unusedField => {
+          delete inputObject[unusedField]
+        })
+        return inputObject
+    }
 }
 
 export const arrayClipper = (inputArray, requiredFields) => {
-    let shuddhArray = []
-    inputArray.forEach(element => {
-        shuddhArray.push(objectClipper(element, requiredFields))
-    })
-
-    return shuddhArray
+    if(inputArray) {
+        let shuddhArray = []
+        inputArray.forEach(element => {
+            shuddhArray.push(objectClipper(element, requiredFields))
+        })
+    
+        return shuddhArray
+    }
 }
